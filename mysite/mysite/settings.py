3bdaf34f92970069535914ac30055287d4cbba5d
Jahseh422 <83466120+jahseh422@users.noreply.github.com>
2021-12-03 10:37:05 +0300

    change some

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -9,12 +9,15 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os.path
 from pathlib import Path
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+PROJECT_DIR = Path(__file__).resolve().parent / 'apps'
+sys.path.append(str(PROJECT_DIR))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
